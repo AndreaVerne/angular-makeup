7a54344ee79dad3fb5f3e05889d5f0ebd3669c9e
Andrea <andrea.verne@hotmail.com>
2022-11-30 11:15:33 -0300

    Clase 3

---
+++ src/app/makeup-list/Makeup.ts
@@ -0,0 +1,8 @@
+export interface Makeup{
+  name: string;
+  category: string;
+  price: number;
+  stock: number;
+  image: string;
+  offer: boolean;
+}
--- src/app/makeup-list/makeup-list.component.ts
+++ src/app/makeup-list/makeup-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Makeup } from './Makeup';
 
 @Component({
   selector: 'app-makeup-list',
@@ -6,13 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./makeup-list.component.scss']
 })
 export class MakeupListComponent implements OnInit {
-makeup = {
-  "name":"Labial Maybelline superStay tono 10 ",
-  "category":"Labios",
-  "price": 1100,
-  "stock": 6,
-  "image": "assets/img/labial-10.jpg"
-}
+makeups: Makeup []= [
+  {
+  name:'Labial Maybelline superStay tono 10',
+  category:'Labios',
+  price: 1100,
+  stock: 6,
+  image: 'assets/img/labial-10.jpg',
+  offer: false,
+  },
+  {
+    name:'Máscara de Pestañas Maybelline Sensational Sky High',
+    category:'Ojos',
+    price: 3970,
+    stock: 0,
+    image: 'assets/img/mascara-sky.jpg',
+    offer: false,
+    },
+    {
+      name:'Base de maquillaje Maybelline Fit Me tono 128',
+      category:'Rostro',
+      price: 4000,
+      stock: 5,
+      image: 'assets/img/base-128.jpg',
+      offer: true,
+      }
+]
   constructor() { }
 
   ngOnInit(): void {
